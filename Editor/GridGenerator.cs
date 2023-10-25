c8e988b57a39403ba849d3d8dd6b0c1528033e1b
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2023-10-25 11:04:25 +0200

    Add GameObjects to the Scene

--- Editor/GridGenerator.cs
+++ Editor/GridGenerator.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class GridGenerator : MonoBehaviour
+namespace Editor
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GridGenerator : EditorWindow
     {
+        private const string ParentName = "Grid";
+        private const string NodeName = "Node";
         
-    }
+        private Vector3 _gridSize = new (0, 0, 0);
+        
+        [MenuItem("Tools/Sok/GridGenerator")]
+        public static void ShowWindow()
+        {
+            GetWindow(typeof(GridGenerator));
+        }
+        
+        private void OnGUI()
+        {
+            GUILayout.Label("Generate Grid", EditorStyles.boldLabel);
+            
+            _gridSize = EditorGUILayout.Vector3Field("Grid Size", _gridSize);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            if (GUILayout.Button("Generate"))
+            {
+                Debug.Log(_gridSize);
+                GenerateGrid();
+            }
+        }
+
+        private void GenerateGrid()
+        {
+            var parent = new GameObject(ParentName);
+            
+            var totalNodes = _gridSize.x + _gridSize.y + _gridSize.z;
+            
+            Debug.Log(totalNodes);
+            
+            for (var i = 0; i < totalNodes; i++)
+            {
+                var node = new GameObject($"{NodeName}_{i}");
+                node.transform.SetParent(parent.transform);
+            }
+        }
     }
 }
