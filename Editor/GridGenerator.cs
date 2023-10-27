294c579519eb2b11589b9394652edaaea2da7b64
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2023-10-27 13:12:22 +0200

    Add GridVisualization.cs

--- Editor/GridGenerator.cs
+++ Editor/GridGenerator.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Editor
 {
@@ -7,11 +6,15 @@ namespace Editor
     {
         private const string ParentName = "Grid";
         private const string NodeName = "Node";
+
+        private Vector3[][][] _nodes;
         
         public void Generate(Vector3 gridSize, float padding)
         {
             var parent = new GameObject(ParentName);
             var gridVisualization = parent.AddComponent<GridVisualization>();
+            gridVisualization.Init(gridSize);
+            
             
             for (var i = 0; i < gridSize.z; i++)
             {
@@ -19,21 +22,18 @@ namespace Editor
                 {
                     for(var k= 0; k < gridSize.x; k++)
                     {
-                        var nodePosition = new Vector3(k, j, i) * padding;
+                        var nodeIndex = new Vector3(k, j, i);
+                        var nodePosition = nodeIndex * padding;
                         var nodeName = $"{NodeName}_{i}_{j}_{k}";
 
                         var node = CreateNode(nodePosition, parent, nodeName);
-                        
-                        var gridData = new GridData(new List<NodeData>(), gridSize);
-                        gridData.Nodes.Add(new NodeData(node.transform));
-                        
-                        gridVisualization.GridData = gridData;
+                        gridVisualization.AddNode(nodeIndex, node);
                     }
                 }
             }
         }
         
-        private GameObject CreateNode(Vector3 nodePosition, GameObject parent, string nodeName)
+        private NodeVisualization CreateNode(Vector3 nodePosition, GameObject parent, string nodeName)
         {
             var node = new GameObject(nodeName)
             {
@@ -42,10 +42,8 @@ namespace Editor
                     position = nodePosition
                 }
             };
-            node.AddComponent<NodeVisualization>();
             node.transform.SetParent(parent.transform);
-
-            return node;
+            return node.AddComponent<NodeVisualization>();
         }
     }
 }
---
+++ Runtime/GridVisualization.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class GridVisualization : MonoBehaviour
+{
+    private NodeVisualization[,,] _nodes;
+    private Vector3 _gridSize;
+
+    public void Init(Vector3 gridSize)
+    { 
+        _gridSize = gridSize;
+        _nodes = new NodeVisualization[(int) gridSize.x,(int) gridSize.y,(int) gridSize.z];
+    }
+    
+    public void AddNode(Vector3 index, NodeVisualization nodeVisualization) 
+        => _nodes[(int) index.x, (int) index.y, (int) index.z] = nodeVisualization;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        for (var i = 0; i < _gridSize.z; i++)
+        {
+            for (var j = 0; j < _gridSize.y; j++)
+            {
+                for (var k = 0; k < _gridSize.x; k++)
+                {
+                    if (k + 1 < _gridSize.x)
+                    {
+                        Gizmos.DrawLine(_nodes[k, j, i].transform.position, _nodes[k + 1, j, i].transform.position);  
+                    }
+                    if(j + 1 < _gridSize.y)
+                    {
+                        Gizmos.DrawLine(_nodes[k, j, i].transform.position, _nodes[k, j + 1, i].transform.position);  
+                    }
+                    if(i + 1 < _gridSize.z)
+                    {
+                        Gizmos.DrawLine(_nodes[k, j, i].transform.position, _nodes[k, j, i + 1].transform.position);  
+                    }
+                }
+            }
+        }
+    }
+}
