632fa1d382ce35c833166c0c5b98907f6bcd1305
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2023-10-27 13:28:05 +0200

    Refactor to avoid nested iterations

--- Runtime/GridVisualization.cs
+++ Runtime/GridVisualization.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 public class GridVisualization : MonoBehaviour
 {
@@ -18,26 +19,22 @@ public class GridVisualization : MonoBehaviour
     {
         Gizmos.color = Color.green;
 
-        for (var i = 0; i < _gridSize.z; i++)
+        var linesToDraw = 
+            from i in Enumerable.Range(0, (int)_gridSize.z)
+            from j in Enumerable.Range(0, (int)_gridSize.y)
+            from k in Enumerable.Range(0, (int)_gridSize.x)
+            let currentNode = _nodes[k, j, i]
+            from neighbor in new[] { (k + 1, j, i), (k, j + 1, i), (k, j, i + 1) }
+            where neighbor.Item1 < _gridSize.x && neighbor.Item2 < _gridSize.y && neighbor.Item3 < _gridSize.z
+            let neighborNode = _nodes[neighbor.Item1, neighbor.Item2, neighbor.Item3]
+            select new { Start = currentNode, End = neighborNode };
+
+        foreach (var line in linesToDraw)
         {
-            for (var j = 0; j < _gridSize.y; j++)
-            {
-                for (var k = 0; k < _gridSize.x; k++)
-                {
-                    if (k + 1 < _gridSize.x)
-                    {
-                        Gizmos.DrawLine(_nodes[k, j, i].transform.position, _nodes[k + 1, j, i].transform.position);  
-                    }
-                    if(j + 1 < _gridSize.y)
-                    {
-                        Gizmos.DrawLine(_nodes[k, j, i].transform.position, _nodes[k, j + 1, i].transform.position);  
-                    }
-                    if(i + 1 < _gridSize.z)
-                    {
-                        Gizmos.DrawLine(_nodes[k, j, i].transform.position, _nodes[k, j, i + 1].transform.position);  
-                    }
-                }
-            }
+            DrawLine(line.Start, line.End);
         }
     }
+    
+    private void DrawLine(NodeVisualization from, NodeVisualization to) 
+        => Gizmos.DrawLine(from.transform.position, to.transform.position);
 }
